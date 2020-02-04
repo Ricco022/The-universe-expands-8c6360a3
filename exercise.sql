0be7df68024cd01cab92c0e8fef8d1e835719588
Riccardo van der Horst <riccardo271002@gmail.com>
2020-02-04 12:56:12 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE Sterrenstelsel;
 CREATE TABLE planeten (naam VARCHAR(20));
+INSERT INTO planeten(naam)
+VALUES ('Zon');
+INSERT INTO planeten(naam)
+VALUES ('Mercurius');
+INSERT INTO planeten(naam)
+VALUES ('Venus');
+INSERT INTO planeten(naam)
+VALUES ('Aarde');
+INSERT INTO planeten(naam)
+VALUES ('Mars');
